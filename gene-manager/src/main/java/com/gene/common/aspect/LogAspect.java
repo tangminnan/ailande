e57561a5cbafcc423472825d48780dc1f94142da
zouying <349829327@qq.com>
2019-10-10 10:44:10 +0800

    ~

--- gene-manager/src/main/java/com/gene/common/aspect/LogAspect.java
+++ gene-manager/src/main/java/com/gene/common/aspect/LogAspect.java
@@ -39,7 +39,7 @@ public class LogAspect {
     LogService logService;
 
 
-    @Pointcut("@annotation(com.yanke.common.annotation.Log)")
+    @Pointcut("@annotation(com.gene.common.annotation.Log)")
     public void logPointCut() {
     }
 
