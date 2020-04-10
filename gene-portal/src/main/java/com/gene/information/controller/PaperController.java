0a7a60e5ec7d2ba2a9467a23b8293506e3240364
zouying <349829327@qq.com>
2020-04-10 22:05:14 +0800

    ~

--- gene-portal/src/main/java/com/gene/information/controller/PaperController.java
+++ gene-portal/src/main/java/com/gene/information/controller/PaperController.java
@@ -60,7 +60,7 @@ public class PaperController {
 		System.out.println("回调执行");
 		System.out.println("回调执行");
 		String code = request.getParameter("code");
-    	String openid = "";//暂定写死，之前是空字符串
+    	String openid = "o85JHw-iXn1_uxz7SgHxdJQf30LU";//暂定写死，之前是空字符串
     	if(StringUtils.isNotBlank(code)){
     		try {
 				openid = WechatOAConfig.getAccessToken(code);
@@ -233,7 +233,7 @@ public class PaperController {
 	public String getReportPage(HttpServletRequest request,HttpServletResponse response,Model model){
 		
 		String code = request.getParameter("code");
-    	String openid = "";//暂定写死，之前是空字符串
+    	String openid = "o85JHw-iXn1_uxz7SgHxdJQf30LU";//暂定写死，之前是空字符串
     	if(StringUtils.isNotBlank(code)){
     		try {
 				openid = WechatOAConfig.getAccessToken(code);
